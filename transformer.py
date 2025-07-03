a4d1f806153f670ca7704d7544600507c1b61464
Lucas Jiao <105329087+lLukii@users.noreply.github.com>
2025-07-03 15:55:56 -0400

    attention can go suck my d

--- transformer.py
+++ transformer.py
@@ -1,16 +1,20 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
 import numpy as np
 
 class PositionalEncoding(nn.Module):
   def __init__(self):
     super(PositionalEncoding, self).__init__()
-  
+
   def forward(self, x: torch.tensor):
-    '''
-    X: [batch_size, seq_len, d_model]
-    '''
+    r"""
+    Computes positional encoding for transformer
+    Parameters:
+      X: [batch_size, seq_len, d_model]
+    """
     batch_size, seq_len, d_model = x.size()
     PE = torch.zeros(batch_size, seq_len, d_model)
     for pos in range(seq_len):
@@ -21,17 +25,18 @@ class PositionalEncoding(nn.Module):
     return x + PE
 
 class MultiHeadAttention(nn.Module):
-  def __init__(self, num_heads: int, d_model: int, attention_size: int, mask=False):
+  def __init__(self, num_heads: int, d_model: int, attention_size: int):
     # man how tf do u vectorize ts 😭
     """
-    num_heads: number of attention heads
-    mask: whether to use masked attention
-    d_model: dimensionality of input
-    attention_size: dimensionality of Q, K, and V vectors
+    Computes multi-head self-attention
+    Parameters:
+      num_heads: number of attention heads
+      mask: whether to use mask
+      d_model: dimensionality of input
+      attention_size: dimensionality of Q, K, and V matricies
     """
     super(MultiHeadAttention, self).__init__()
     self.num_heads = num_heads
-    self.mask = mask
     self.attention_size = attention_size
     self.linear_K = [nn.Linear(d_model, attention_size)] * num_heads
     self.linear_V = [nn.Linear(d_model, attention_size)] * num_heads
@@ -39,37 +44,66 @@ class MultiHeadAttention(nn.Module):
 
     self.linear_O = nn.Linear(num_heads * attention_size, d_model)
 
-  def forward(self, x: torch.tensor):
+  def forward(self, x: torch.tensor, mask : bool = False):
     '''
-    X: [batch_size, seq_len, d_model]
+    Parameters:
+      X: [batch_size, seq_len, d_model]
+      mask: bool whether or not to use mask
     '''
     batch_size, seq_len, d_model = x.size()
-    attention = torch.zeros(batch_size, seq_len, self.num_heads, self.attention_size)
+    attention = torch.zeros(batch_size, self.num_heads, seq_len, self.attention_size)
+    mask  = torch.tril(torch.ones(seq_len, seq_len)) if mask else None
     for head in range(self.num_heads):
       K = self.linear_K[head](x)
       V = self.linear_V[head](x)
       Q = self.linear_Q[head](x)
-      attention_head = F.scaled_dot_product_attention(Q, K, V) # softmax(QK^T / sqrt(d_k))V
-      attention[:, :, head, :] = attention_head
+      attention_head = F.scaled_dot_product_attention(Q, K, V,
+                                                      attn_mask=mask) # ok lil bro
+      attention[:, head, :, :] = attention_head
 
     attention = torch.reshape(attention, (batch_size, seq_len, -1)) # num_heads * attention_size
     attention = self.linear_O(attention)
     return x + attention
 
+
+class MHCrossAttention(nn.Module):
+  def __init__(self, num_heads: int, d_model: int, attention_size: int):
+    super(MHCrossAttention, self).__init__()
+    self.num_heads = num_heads
+    self.attention_size = attention_size
+    self.linear_K = [nn.Linear(d_model, attention_size)] * num_heads
+    self.linear_V = [nn.Linear(d_model, attention_size)] * num_heads
+    self.linear_Q = [nn.Linear(d_model, attention_size)] * num_heads
+
+    self.linear_O = nn.Linear(num_heads * attention_size, d_model)
+  
+  def forward(self, encoder: torch.tensor, decoder: torch.tensor):
+    batch_size, _, _ = encoder.size()
+    _, seq_len, _ = decoder.size()
+    attention = torch.zeros(batch_size, self.num_heads, seq_len, self.attention_size)
+    for head in range(self.num_heads):
+      Q = self.linear_Q[head](decoder)
+      K = self.linear_K[head](encoder)
+      V = self.linear_V[head](encoder)
+      attention_head = F.scaled_dot_product_attention(Q, K, V) # softmax(QK^T / sqrt(d_k))V
+      attention[:, head, :, :] = attention_head 
+    
+    attention = torch.reshape(attention, (batch_size, seq_len, -1)) # num_heads * attention_size
+    attention = self.linear_O(attention)
+    return decoder + attention
+  
 class FFNLayer(nn.Module):
-  def __init__(self, input_size, hidden_size, output_size, dropout_rate=0.1):
+  def __init__(self, input_size, hidden_size, dropout_rate=0.1):
     super(FFNLayer, self).__init__()
     self.ffn = nn.Sequential(
         nn.Linear(input_size, hidden_size),
         nn.ReLU(),
         nn.Dropout(dropout_rate),
-        nn.Linear(hidden_size, output_size),
+        nn.Linear(hidden_size, input_size),
     )
 
   def forward(self, x):
-    '''
-    X: [batch_size, seq_len, d_model]
-    '''
+    ''' X: [batch_size, seq_len, d_model] '''
     return self.ffn(x)
 
 class Encoder(nn.Module):
@@ -79,7 +113,7 @@ class Encoder(nn.Module):
     self.ffn = FFNLayer(d_model, hidden_size)
     self.norm = nn.LayerNorm(d_model)
     self.pe = PositionalEncoding()
-  
+
   def forward(self, x: torch.tensor, n_x: int):
     '''
     X: [batch_size, seq_len, d_model]
@@ -94,7 +128,6 @@ class Encoder(nn.Module):
       x = self.norm(x + tmp)
     return x
 
-
 class Decoder(nn.Module):
   def __init__(self, num_heads=8, d_model=512, attention_size=64, hidden_size=2048):
     super(Decoder, self).__init__()
@@ -102,7 +135,7 @@ class Decoder(nn.Module):
     self.ffn = FFNLayer(d_model, hidden_size)
     self.norm = nn.LayerNorm(d_model)
     self.pe = PositionalEncoding()
-  
+
   def forward(self, x: torch.tensor, n_x: int):
     '''
     X: [batch_size, seq_len, d_model]
@@ -118,13 +151,11 @@ class Decoder(nn.Module):
       tmp = torch.clone(x)
       x = self.ffn(x)
       x = self.norm(x + tmp)
-    
-    return x
 
+    return x
 
 class Transformer(nn.Module):
   def __init__(self):
     super(Transformer, self).__init__()
     self.encoder = Encoder()
     self.decoder = Decoder()
-
